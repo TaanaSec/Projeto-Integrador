24978ed32ce1e8bb7544bee59bad345fdaa33033
= <=>
2026-08-03 10:45:57 -0300

    Rota "/contato" adicionada
    Adiciona comentários para demais rotas

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -7,8 +7,13 @@ const router = express.Router()
 // Cadastrar
 router.post('/cadastro', loginController.cadastro)
 
+// Login
 router.post('/login', loginController.login)
 
+// Página do perfil do usuário (privado)
 router.get('/perfil', auth, loginController.perfil)
+
+// Página de contato (privado)
+router.get('/contato', auth, loginController.contato)
 
 module.exports = router
