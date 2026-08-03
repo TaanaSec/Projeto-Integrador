b693d6bd5194bf374c3c11f660494e6282817c9e
= <=>
2026-08-03 11:40:45 -0300

    Cria script de enviar e-mail
    Mescla os dois arquivos de enviar email e de contato (POST e GET)
    
    * Falta: finalizar contato.html;  Adicionar a rota de enviar email.

---
+++ JS/contato/script.js
@@ -0,0 +1,67 @@
+const token = sessionStorage.getItem('token')
+
+function mostrarMensagem(texto, tipo = 'erro') {
+    const el = document.getElementById('mensagem')
+    if (!el) return
+    el.innerHTML = texto
+    el.className = `mensagem ${tipo}`
+}
+
+// Verifica o token
+if (!token) {
+    window.location.href = '../pages/login.html'
+}
+
+
+// Usuário acessa a página
+fetch('http://localhost:3000/contato', {
+    method: 'GET',
+    headers: {
+        Authorization: `Bearer ${token}`
+    }
+})
+.then(response => {
+    if (response.status === 401) {
+        sessionStorage.clear()
+        window.location.href = '../pages/login.html'
+        return
+    }
+
+    return response.json()
+})
+.then(data => {
+    document.getElementById('nomeUsuario').textContent = data.nome
+    document.getElementById('emailUsuario').textContent = data.email
+})
+
+
+// Função de enviar o e-mail
+function enviarEmail() {
+    const para = document.getElementById('para').value
+    const assunto = document.getElementById('assunto').value
+    const texto = document.getElementById('texto').value
+    const html = document.getElementById('html').value
+
+    if (!para || !assunto || !texto || !html) {
+        mostrarMensagem("Pro favor, preencha todos os campos.", 'erro')
+        return
+    }
+
+    fetch ('http://localhost:3000/contato', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}}`
+        },
+        body: JSON.stringify({ para, assunto, texto, html })
+    })
+    .then(res => {
+        if (res.ok) {
+            mostrarMensagem("Email enviado com sucesso!", 'sucesso')
+        }
+    })
+    .catch (error => {
+        console.error("Erro ao enviar email:", error)
+        mostrarMensagem("Erro ao enviar email.", 'erro')
+    })
+}
