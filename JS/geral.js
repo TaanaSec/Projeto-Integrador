ab4fd93a4771f8a2458525760ccd3635cba98e47
= <=>
2026-08-06 11:16:03 -0300

    Muda o href para o /perfil quando usuário logado
    Add o id="linkPerfil" no link

--- JS/geral.js
+++ JS/geral.js
@@ -1,3 +1,4 @@
+// Troca o texto para o nome do usuário logado e o link
 document.addEventListener('DOMContentLoaded', () => {
     const usuario = JSON.parse(
     sessionStorage.getItem('usuarioLogado')
@@ -5,8 +6,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const perfilLogado = document.getElementById('perfilLogado')
 
+    const linkPerfil = document.getElementById('linkPerfil')
+
     if (usuario) {
     perfilLogado.textContent = `Olá, ${usuario.nome}`
     }
 
+    linkPerfil.href = '../pages/perfil.html'
 })
