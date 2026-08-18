343e0f6313a608acf7dd1ce83ae8ed9feff24789
TaanaSec <cys.mariana@gmail.com>
2026-08-18 11:39:51 -0300

    Implementado CRUD de Relatórios Criadas rotas GET, POST, PUT e DELETE Integrado MongoDB com Mongoose Adicionado status ativo/excluído Implementada listagem e renderização dos relatórios Implementados pesquisa e filtro por tipo Implementadas edição e exclusão Adicionados contadores de relatórios Ajustado formulário para criar/editar Adicionado estilo dos cards e ações

--- models/relatoriosModel.js
+++ models/relatoriosModel.js
@@ -7,8 +7,12 @@ const relatorioSchema = new mongoose.Schema({
     dataFinal: {type: Date, required: true},
     descricao: {type: String, required: true},
     status: {type: String, enum: ['ativo', 'excluido'], default: 'ativo'}
-})
+},
+{
+    timestamps: true //O sistema define a data e a hora exatas quando o dado é salvo pela primeira vez
+}
+)
 
-const Relatorio = mongoose.model('Relatorio', relatorioSchema)
+const Relatorio = mongoose.model('Relatorio', relatorioSchema, 'Relatorios')
 
 module.exports = Relatorio
