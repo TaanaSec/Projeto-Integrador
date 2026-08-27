4fa393355c135454fc0ec2c2ac4fa50f24ef090c
= <93229005+davi-marques@users.noreply.github.com>
2026-08-26 22:48:12 -0300

    Corrige scrtip para não falhar em páginas sem logout

--- JS/geral.js
+++ JS/geral.js
@@ -1,22 +1,23 @@
 // Troca o texto para o nome do usuário logado e o link
 document.addEventListener('DOMContentLoaded', () => {
-    const usuario = JSON.parse(
-    sessionStorage.getItem('usuarioLogado')
-    )
+    const usuarioSalvo = sessionStorage.getItem('usuarioLogado')
+    const usuario = usuarioSalvo ? JSON.parse(usuarioSalvo) : null
 
     const perfilLogado = document.getElementById('perfilLogado')
 
     const linkPerfil = document.getElementById('linkPerfil')
 
-    if (usuario) {
-    perfilLogado.textContent = `Olá, ${usuario.nome}`
+    if (usuario && perfilLogado) {
+        perfilLogado.textContent = `Olá, ${usuario.nome}`
     }
 
-    linkPerfil.href = '../pages/perfil.html'
+    if (usuario && linkPerfil) {
+        linkPerfil.href = '../pages/perfil.html'
+    }
 })
 
 // Botão de deslogar
-document.getElementById('btnLogout').addEventListener('click', () => {
+document.getElementById('btnLogout')?.addEventListener('click', () => {
     sessionStorage.clear()
 
     window.location.href = '../pages/login.html'
